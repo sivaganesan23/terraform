b2e5dbceaaee8a1a7f255fc63ad917a56a26cf93
Raghu K <raghuk.vit@gmail.com>
2018-12-17 08:03:47 +0530

    ss

--- stack/modules.tf
+++ stack/modules.tf
@@ -12,5 +12,5 @@ module "database" {
     source = "./Modules/database"
     proj            = "${var.proj}"
     application     = "${var.application}"
-    private_subnets = "${module.infra.private_subnets}"
+    private_subnets = "${module.network.private_subnets}"
 }
