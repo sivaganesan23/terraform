6dbc9f0b0d2b21757cbef3efadf713e0301d63d9
Raghu K <raghuk.vit@gmail.com>
2018-12-17 07:41:42 +0530

    ss

--- 09-module-references/Modules/b/main.tf
+++ 09-module-references/Modules/b/main.tf
@@ -1,7 +1,7 @@
 variable "vpc_id" {}
 
 resource "aws_subnet" {
-    vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
+    vpc_id     = "${var.vpc_id}"
+    cidr_block = "10.0.1.0/24"
 
 } 
