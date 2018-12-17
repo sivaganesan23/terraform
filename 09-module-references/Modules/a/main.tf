b90ca1633c8a02a90bddcdfb3794039862e4bf08
Raghu K <raghuk.vit@gmail.com>
2018-12-17 07:31:27 +0530

    ss

--- 09-module-references/Modules/a/main.tf
+++ 09-module-references/Modules/a/main.tf
@@ -2,3 +2,11 @@ variable "aws_vpc" {
     cidr_block = "10.5.0.0/22"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
+}
