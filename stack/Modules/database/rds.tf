fe510afb9b4c16d22cf3e3e2129522165247e060
Raghu K <raghuk.vit@gmail.com>
2018-12-17 08:15:56 +0530

    ss

--- stack/Modules/database/rds.tf
+++ stack/Modules/database/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_parameter_group" "default" {
 }
 
 resource "aws_db_subnet_group" "default" {
-  name       = "main"
+  name       = ""
   subnet_ids = ["${var.private_subnets}"]
 
   tags = {
