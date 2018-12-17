5d4cb4894e5336774face8f342bcaaef48c2a47c
Raghu K <raghuk.vit@gmail.com>
2018-12-17 07:59:11 +0530

    ss

---
+++ stack/modules.tf
@@ -0,0 +1,16 @@
+module "network" {
+    source          = "./Modules/network"
+    proj            = "${var.proj}"
+    application     = "${var.application}"
+}
+
+module "infra" {
+    source          = "./Modules/infra"
+}
+
+module "database" {
+    source = "./Modules/database"
+    proj            = "${var.proj}"
+    application     = "${var.application}"
+    private_subnets = "${module.infra.private_subnets}"
+}
