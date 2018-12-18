18718d6ec1b3463aa2be6d25db257850dafc4ae6
Raghu K <raghuk.vit@gmail.com>
2018-12-18 07:31:36 +0530

    ss

--- stack/Modules/network/vpc.tf
+++ stack/Modules/network/vpc.tf
@@ -31,7 +31,7 @@ resource "aws_vpc_peering_connection" "peer" {
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
-  provider                  = "aws.peer"
+  provider                  = "aws"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
   auto_accept               = true
 
