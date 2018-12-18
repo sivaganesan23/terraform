0202df44bd5740f9fdf24e0266740d2f78be3053
Raghu K <raghuk.vit@gmail.com>
2018-12-18 07:39:40 +0530

    ss

--- stack/Modules/network/vpc.tf
+++ stack/Modules/network/vpc.tf
@@ -108,3 +108,9 @@ resource "aws_route_table_association" "priv-rta" {
 
 
 ### Add route entry to existing VPC with peering connection
+resource "aws_route" "r" {
+  route_table_id            = "rtb-4fbb3ac4"
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+  depends_on                = ["aws_route_table.testing"]
+}
