2bccfe56a797ede57d4512e8a08683718471692b
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:20:59 +0530

    ss

--- stack/Modules/infra/clb.tf
+++ stack/Modules/infra/clb.tf
@@ -18,7 +18,7 @@ resource "aws_elb" "app-elb" {
     interval            = 30
   }
 
-  instances                   = ["${aws_instance.a.id}"]
+  instances                   = ["${aws_instance.server.*.id}"]
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
