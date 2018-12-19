933a15c66d658322c572de623b9eb07e861a7d5d
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:20:34 +0530

    ss

--- stack/Modules/infra/clb.tf
+++ stack/Modules/infra/clb.tf
@@ -10,19 +10,11 @@ resource "aws_elb" "app-elb" {
     lb_protocol       = "http"
   }
 
-  listener {
-    instance_port      = 8000
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
-  }
-
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:8000/"
+    target              = "HTTP:80/"
     interval            = 30
   }
 
