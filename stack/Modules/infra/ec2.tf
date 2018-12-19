d1e8d8f1e1b4a82eb7d2fc272ae876a5b3be49a1
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:00:23 +0530

    ss

--- stack/Modules/infra/ec2.tf
+++ stack/Modules/infra/ec2.tf
@@ -42,7 +42,7 @@ resource "null-resource" "appsetup" {
     provisioner "remote-exec" {
         inline = [
         "sudo yum install ansible git -y",
-        "ansible-pull -U https://github.com/citb33/ansible-pull.git ",
+        "ansible-pull -U https://github.com/citb33/ansible-pull.git webapp.yml",
         ]
   }
 }
