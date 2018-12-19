22e5221e5adf7b3b0e66cf3a22a9760f50f45d57
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:00:59 +0530

    ss

--- stack/Modules/infra/ec2.tf
+++ stack/Modules/infra/ec2.tf
@@ -42,7 +42,7 @@ resource "null-resource" "appsetup" {
     provisioner "remote-exec" {
         inline = [
         "sudo yum install ansible git -y",
-        "ansible-pull -U https://github.com/citb33/ansible-pull.git webapp.yml -e DBUSER DBPASS",
+        "ansible-pull -U https://github.com/citb33/ansible-pull.git webapp.yml -e DBUSER DBPASS  DBIP DBNAME",
         ]
   }
 }
