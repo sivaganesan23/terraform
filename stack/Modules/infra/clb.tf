2aba7bc7f91a995090090374a4a2e0f204b351f3
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:31:16 +0530

    ss

--- stack/Modules/infra/clb.tf
+++ stack/Modules/infra/clb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "app-elb" {
   name               = "${var.proj}-app-clb"
-  availability_zones = ["us-east-2a"]
+  availability_zones = ["us-east-2a", "us-east-2b"]
 
 
   listener {
