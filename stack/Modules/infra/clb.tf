4ae69013720c241acf0abaf03001aa791faf74d5
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:20:48 +0530

    ss

--- stack/Modules/infra/clb.tf
+++ stack/Modules/infra/clb.tf
@@ -14,7 +14,7 @@ resource "aws_elb" "app-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "TR:80"
+    target              = "TCP:80"
     interval            = 30
   }
 
