41ff222ae13c6a82c9f770128110771678ff8992
Raghu K <raghuk.vit@gmail.com>
2018-12-19 08:05:09 +0530

    ss

--- stack/Modules/infra/ec2.tf
+++ stack/Modules/infra/ec2.tf
@@ -49,7 +49,7 @@ resource "null_resource" "appsetup" {
             type        = "ssh"
             user        = "centos"
             private_key = "${file("/home/centos/devops.pem")}"
-            host        = ""
+            host        = "${}"
         }
   }
 }
