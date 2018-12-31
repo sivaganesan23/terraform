da8604846fbbbe399c5645f4dc0880bbd17f9ed1
Raghu K <raghuk.vit@gmail.com>
2018-12-31 02:13:14 +0000

    Updating terraform

--- stack-test-env/Modules/infra/ec2.tf
+++ stack-test-env/Modules/infra/ec2.tf
@@ -59,3 +59,8 @@ resource "local_file" "ipaddress_of_ec2" {
     content     = "${element(aws_instance.server.*.private_ip, 0)}"
     filename = "/tmp/hosts"
 }
+
+resource "local_file" "public_address_of_ec2" {
+    content     = "${element(aws_instance.server.*.public_ip, 0)}"
+    filename = "/tmp/pubip"
+}
